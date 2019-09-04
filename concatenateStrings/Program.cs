5f349d2deb623d6c119ad82cb2891bea2436739c
jweaver156 <46185473+jweaver156@users.noreply.github.com>
2019-09-03 18:29:49 -0700

    Add files via upload

---
+++ concatenateStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concatenateStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "This is my first string,";
+            string second = " this is my second string,";
+            string third = " and this is my third string!!!.";
+
+            third = third.ToUpper();
+            string cString = first + second + third;
+            StringBuilder fString = new StringBuilder(cString);
+            fString.Append("\n...and finally my fourth string");
+            Console.WriteLine(fString); 
+            Console.ReadLine();
+
+            
+        }
+    }
+}
